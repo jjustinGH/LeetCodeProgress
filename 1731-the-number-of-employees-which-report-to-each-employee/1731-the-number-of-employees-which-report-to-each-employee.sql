cb2ebad6b593388afa0b5c80d24382c883a1408e
jjustinGH <justinlu73@gmail.com>
2026-05-08 15:11:43 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select a.employee_id, a.name, count(b.reports_to) as reports_count, round(avg(b.age),0) as average_age from employees a left join employees b on a.employee_id = b.reports_to group by a.employee_id having reports_count >= 1 order by a.employee_id
+select a.employee_id, a.name, count(b.reports_to) as reports_count, round(avg(b.age),0) as average_age from employees a join employees b on a.employee_id = b.reports_to group by a.employee_id  order by a.employee_id
