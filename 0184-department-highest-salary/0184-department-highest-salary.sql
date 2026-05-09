428c61c3657b24e6b02e40cfb221ced824146dd9
jjustinGH <justinlu73@gmail.com>
2026-05-09 16:15:17 +0800

    Time: 672 ms (56.9%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,3 +1,2 @@
 # Write your MySQL query statement below
-with view as (select *, dense_rank() over (partition by departmentid order by salary desc) as rankss from employee)
-select d.name as department, v.name as employee, v.salary from view v join department d on v.departmentid = d.id where rankss = 1
+select d.name as department, e.name as employee, e.salary from employee e left join department d on e.departmentid = d.id where (e.departmentid, e.salary) in (select departmentid, max(salary) from employee group by departmentid )
