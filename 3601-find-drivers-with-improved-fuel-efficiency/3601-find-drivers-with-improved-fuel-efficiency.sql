181f312b4ced6d03ee99e530c19f17381f4b3b5f
jjustinGH <justinlu73@gmail.com>
2026-05-28 14:52:55 +0800

    Time: 559 ms (43.76%), Space: 0B (100%) - LeetHub

---
+++ 3601-find-drivers-with-improved-fuel-efficiency/3601-find-drivers-with-improved-fuel-efficiency.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+with first_half as (
+    select driver_id, avg(distance_km / fuel_consumed) as first_half_avg from trips where month(trip_date) between 1 and 6 group by driver_id
+),
+sec_half as (
+    select driver_id, avg(distance_km / fuel_consumed) as second_half_avg from trips where month(trip_date) between 7 and 12 group by driver_id
+)
+select f.driver_id, d.driver_name, round(f.first_half_avg,2) as first_half_avg, round(s.second_half_avg,2) as second_half_avg, round(s.second_half_avg - f.first_half_avg,2) as efficiency_improvement from first_half f join sec_half s on f.driver_id = s.driver_id join drivers d on f.driver_id = d.driver_id group by f.driver_id having efficiency_improvement > 0 order by efficiency_improvement desc, d.driver_name 
