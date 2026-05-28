e2a54ff6597d397e6bc7f888dda36bd7b7e2791a
jjustinGH <justinlu73@gmail.com>
2026-05-28 15:00:32 +0800

    Time: 741 ms (7.6%), Space: 0B (100%) - LeetHub

--- 3601-find-drivers-with-improved-fuel-efficiency/3601-find-drivers-with-improved-fuel-efficiency.sql
+++ 3601-find-drivers-with-improved-fuel-efficiency/3601-find-drivers-with-improved-fuel-efficiency.sql
@@ -1,8 +1,6 @@
 # Write your MySQL query statement below
-with first_half as (
-    select driver_id, avg(distance_km / fuel_consumed) as first_half_avg from trips where month(trip_date) between 1 and 6 group by driver_id
-),
-sec_half as (
-    select driver_id, avg(distance_km / fuel_consumed) as second_half_avg from trips where month(trip_date) between 7 and 12 group by driver_id
+with view as (
+    select driver_id, avg(case when month(trip_date) between 1 and 6 then distance_km / fuel_consumed end) as first_half_avg, avg(case when month(trip_date) between 7 and 12 then distance_km / fuel_consumed end) as second_half_avg from trips group by driver_id
 )
-select f.driver_id, d.driver_name, round(f.first_half_avg,2) as first_half_avg, round(s.second_half_avg,2) as second_half_avg, round(s.second_half_avg - f.first_half_avg,2) as efficiency_improvement from first_half f join sec_half s on f.driver_id = s.driver_id join drivers d on f.driver_id = d.driver_id group by f.driver_id having efficiency_improvement > 0 order by efficiency_improvement desc, d.driver_name 
+select v.driver_id, d.driver_name, round(v.first_half_avg,2) as first_half_avg, round(v.second_half_avg,2) as second_half_avg, round(v.second_half_avg - v.first_half_avg,2) as efficiency_improvement from view v join drivers d on v.driver_id = d.driver_id where v.first_half_avg is not null and v.second_half_avg is not null and v.second_half_avg - v.first_half_avg > 0 order by efficiency_improvement desc, d.driver_name
+#select f.driver_id, d.driver_name, round(f.first_half_avg,2) as first_half_avg, round(s.second_half_avg,2) as second_half_avg, round(s.second_half_avg - f.first_half_avg,2) as efficiency_improvement from first_half f join sec_half s on f.driver_id = s.driver_id join drivers d on f.driver_id = d.driver_id group by f.driver_id having efficiency_improvement > 0 order by efficiency_improvement desc, d.driver_name 
