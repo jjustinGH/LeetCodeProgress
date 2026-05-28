8c1abd958410e51ea340c50f93a4799bfb1757ed
jjustinGH <justinlu73@gmail.com>
2026-05-28 14:03:26 +0800

    Time: 578 ms (27.69%), Space: 0B (100%) - LeetHub

--- 3580-find-consistently-improving-employees/3580-find-consistently-improving-employees.sql
+++ 3580-find-consistently-improving-employees/3580-find-consistently-improving-employees.sql
@@ -1,2 +1,8 @@
 # Write your MySQL query statement below
-select t.employee_id, e.name, sum(case when t.rnk = 1 then t.rating else 0 end) - sum(case when t.rnk = 3 then t.rating else 0 end) as improvement_score from (select employee_id, review_date, rating, row_number() over(partition by employee_id order by review_date desc) as rnk from performance_reviews) t left join employees e on t.employee_id = e.employee_id group by employee_id having count(rnk) >= 3 and improvement_score > 0 and sum(case when t.rnk = 1 then t.rating else 0 end) - sum(case when t.rnk = 2 then t.rating else 0 end) > 0 and sum(case when t.rnk = 2 then t.rating else 0 end) - sum(case when t.rnk = 3 then t.rating else 0 end) > 0 order by improvement_score desc, e.name
+with ranked as (
+    select employee_id, rating, row_number() over(partition by employee_id order by review_date desc) as rnk from performance_reviews
+),
+score as (
+    select employee_id, sum(case when rnk = 1 then rating end) as r1, sum(case when rnk = 2 then rating end) as r2, sum(case when rnk = 3 then rating end) as r3 from ranked group by employee_id having count(rnk) >= 3 and r1 > r2 and r2 > r3 and r1 - r3 > 0
+)
+select s.employee_id, e.name, r1-r3 as improvement_score from score s join employees e on s.employee_id = e.employee_id order by improvement_score desc, e.name
