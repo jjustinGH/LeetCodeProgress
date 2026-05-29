b1c621d9f09ae57b4ab7f055ed6d2be84f8b08b8
jjustinGH <justinlu73@gmail.com>
2026-05-29 17:49:26 +0800

    Time: 335 ms (47.52%), Space: 0B (100%) - LeetHub

--- 3657-find-loyal-customers/3657-find-loyal-customers.sql
+++ 3657-find-loyal-customers/3657-find-loyal-customers.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select customer_id from customer_transactions group by customer_id having count(*) >= 3 and datediff(max(transaction_date), min(transaction_date)) >= 30 and sum(case when transaction_type = 'refund' then 1 else 0 end) / count(*) < 0.2 order by customer_id
+select customer_id from customer_transactions group by customer_id having count(*) >= 3 and datediff(max(transaction_date), min(transaction_date)) >= 30 and sum(transaction_type = 'refund') / count(*) < 0.2 order by customer_id
